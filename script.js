587b51fefba0156009433d6b6a97321db5ea40aa
AlexeyZl <89382676+AlexeyZl@users.noreply.github.com>
2023-02-23 20:11:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -150,7 +150,7 @@
 // const arr = []
 // for (let i = 1; i <= 50; ++i) {
 //     arr.push(i)
-       
+
 // }
 //  //console.log(arr);
 
@@ -183,7 +183,7 @@
 // //expression
 
 // const wowScream = function () {
-    
+
 // }
 
 // //arrow
@@ -200,26 +200,26 @@
 //сменить кнопку на "начать заново"
 
 const getRandomNumInRange = (min, max) => {
-    const randomNum = (Math.random() * (max - min) + min ).toFixed(0)
+    const randomNum = (Math.random() * (max - min) + min).toFixed(0)
     return randomNum
 }
 
 const getTask = () => {
-//    const randomNum1 = getRandomNumInRange(0, 100)
-//    const randomNum2 = getRandomNumInRange(0, 100) 
-//    let symbol
-//    if (Math.random() > 0.5) {
-//        symbol = "+" 
-//    } else {
-//        symbol = "-"
-//    }
-   const symbol = (Math.random() > 0.5) ? "+" : "-"
-   const task = `${getRandomNumInRange(0, 100)} ${symbol} ${getRandomNumInRange(0, 100)}`
-   gameState.rightAnswer = eval(task)
-   return task
+    //    const randomNum1 = getRandomNumInRange(0, 100)
+    //    const randomNum2 = getRandomNumInRange(0, 100) 
+    //    let symbol
+    //    if (Math.random() > 0.5) {
+    //        symbol = "+" 
+    //    } else {
+    //        symbol = "-"
+    //    }
+    const symbol = (Math.random() > 0.5) ? "+" : "-"
+    const task = `${getRandomNumInRange(0, 100)} ${symbol} ${getRandomNumInRange(0, 100)}`
+    gameState.rightAnswer = eval(task)
+    return task
 }
 
-const toggleGameState = () =>{
+const toggleGameState = () => {
 
 }
 
@@ -238,7 +238,7 @@ const gameState = {
 }
 
 const startGameFunc = () => {
-    if(!gameState.taskInProcess) {
+    if (!gameState.taskInProcess) {
         title.innerText = "Игра началась!"
         userAnswer.value = null
         //генерируем задачу и ответ
@@ -264,12 +264,12 @@ const startGameFunc = () => {
 }
 btnGame.addEventListener('click', startGameFunc)
 userAnswer.addEventListener('keydown', (e) => {
-  //  console.log(e);
-  if (e.key === "Enter") {
-    startGameFunc()
-  } else if (e.key === "Escape"){
-      userAnswer.blur()
-  }
+    //  console.log(e);
+    if (e.key === "Enter") {
+        startGameFunc()
+    } else if (e.key === "Escape") {
+        userAnswer.blur()
+    }
 })
 
 
@@ -282,28 +282,104 @@ const choosedState = {
     countElements: 0,
 }
 const changeCount = (value) => {
-    choosedState.countElements  += value
+    choosedState.countElements += value
     counterEl.innerText = choosedState.countElements
-} 
+}
 
 const eventFunc = (e) => {
 
-        //выбрать элемент, т.е. выделить его с помощью класса
-        //choosedEl[i].className = 'choosed_element'
-        //console.log(e);
-        //e.target.className = 'choosed_element'
-        //запустить счетчик
-        //counterEl.innerText = +counterEl.innerText + 1
-        if (e.target.className === ""){
-            e.target.className = 'choosed_element'
-            changeCount(1)
-        } else{
-            e.target.className = ''
-            changeCount(-1)
-        }
+    //выбрать элемент, т.е. выделить его с помощью класса
+    //choosedEl[i].className = 'choosed_element'
+    //console.log(e);
+    //e.target.className = 'choosed_element'
+    //запустить счетчик
+    //counterEl.innerText = +counterEl.innerText + 1
+    if (e.target.className === "") {
+        e.target.className = 'choosed_element'
+        changeCount(1)
+    } else {
+        e.target.className = ''
+        changeCount(-1)
     }
+}
 
 for (let i = 0; i < choosedEl.length; i++) {
-    choosedEl[i].addEventListener("click", eventFunc)  
+    choosedEl[i].addEventListener("click", eventFunc)
 }
-choosedEl[2].removeEventListener("click", eventFunc)
+//choosedEl[2].removeEventListener("click", eventFunc)
+
+const timeIsOver = () => {
+    alert("Время вышло!")
+}
+//setTimeout(timeIsOver, 2000)
+
+//const alarm = setInterval (timeIsOver, 2000)
+
+
+// const alarm = setInterval(() => {
+// let wantSleep = confirm('Хотите спать?')
+// if (wantSleep) {
+//     console.log('tic');
+// } else {
+//     clearInterval(alarm)
+// }
+// }, 3000)
+
+//clearInterval(alarm)
+
+// console.log("1");
+// setTimeout(() =>{
+//     console.log("2"); 
+// }, 0)
+// console.log("3");
+const postsBlock = document.querySelector(".posts_block-container")
+const showPostsBTN = document.querySelector(".posts_block button")
+
+
+
+
+function addPost(title, body) {
+    const postsTitle = document.createElement("h3")
+    const postsBody = document.createElement("span")
+    const postItem = document.createElement("p")
+
+    postsTitle.innerText = title
+    postsBody.innerText = body
+
+
+    postItem.append(postsTitle, postsBody)
+    postsBlock.append(postItem)
+}
+function getPost() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => {
+        return res.json()
+    })
+    .then(data => {
+        for(item of data) {
+            addPost(item.title, item.body)
+        }
+    })
+    .catch((err) => console.log(err.message))
+}
+
+// function createPost(title, body, userId) {
+//     fetch('https://jsonplaceholder.typicode.com/posts', {
+//   method: 'POST',
+//   body: JSON.stringify({ 
+//     title,
+//     body,
+//     userId,
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   }, 
+// })
+// .then( res => {
+//     onsole.log(res)
+//     return res.json()
+// })
+// }
+// createPost('title', 'body', 15)
+
+showPostsBTN.onclick = getPost
